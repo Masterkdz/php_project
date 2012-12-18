37014cbcb32b06e46c2d7103cb97d728b00b8850
Infinita <infinita740@gmail.com>
2012-12-18 20:11:55 +0100

    menu dans un fichir séparé

--- index.php
+++ index.php
@@ -19,20 +19,9 @@
 		</tr>
 		</form>
 	</table>
-<nav>
-	<ul>
-		<a href='index.php'><li><img src="img/testcaret.png">Accueil</li></a>
-		<a href='#'><li><img src="img/testcaret.png">Nouveaut&eacute;s</li></a>
-		<a href='#'><li><img src="img/testcaret.png">Bestiaire</li></a>
-		<a href='#'><li><img src="img/testcaret.png">Boutique</li></a>
-		<a href='#'><li><img src="img/testcaret.png">Extras</li></a>
-		<a href='livreor.htm'><li><img src="img/testcaret.png">Livre d'or</li></a>
-		<a href='#'><li><img src="img/testcaret.png">Contact</li></a>
-		<a href='pageinscription.php'><li><img src="img/testcaret.png">Inscrivez-vous</li></a>
-</nav>
-
 
 <?php
+include("menu.php");
 echo "<h1>It works!</h1>";?>
 <p>
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
---
+++ menu.php
@@ -0,0 +1,11 @@
+<nav>
+	<ul>
+		<a href='index.php'><li><img src="img/testcaret.png">Accueil</li></a>
+		<a href='#'><li><img src="img/testcaret.png">Nouveaut&eacute;s</li></a>
+		<a href='#'><li><img src="img/testcaret.png">Bestiaire</li></a>
+		<a href='#'><li><img src="img/testcaret.png">Boutique</li></a>
+		<a href='#'><li><img src="img/testcaret.png">Extras</li></a>
+		<a href='livreor.htm'><li><img src="img/testcaret.png">Livre d'or</li></a>
+		<a href='#'><li><img src="img/testcaret.png">Contact</li></a>
+		<a href='pageinscription.php'><li><img src="img/testcaret.png">Inscrivez-vous</li></a>
+</nav>
