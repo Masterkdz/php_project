610080df50c749354ba5a16a8edfcac4d0835d03
Infinita <infinita740@gmail.com>
2012-12-18 01:35:22 +0100

    favicon (fail)

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 <html>
 <head><title>Site du projet</title></head>
 <link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="icon" type="image/png" href="/img/game_pad.png" />
 <body>
-
-<?php
-echo "<h1>It works!</h1>";?>
 <nav>
 	<ul>
 		<a href='#'><li>Accueil</li></a>
@@ -16,5 +14,11 @@ echo "<h1>It works!</h1>";?>
 		<a href='#'><li>Livre d'or</li></a>
 		<a href='#'><li>Contact</li></a>
 </nav>
+
+<?php
+echo "<h1>It works!</h1>";?>
+<p>
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+</p>
 </body>
 </html>
