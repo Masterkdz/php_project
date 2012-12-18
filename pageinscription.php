6a49aab8a9c8e2ef9e55d353381cee57cf926991
Infinita <infinita740@gmail.com>
2012-12-18 15:18:54 +0100

    messages d'erreurs

--- pageinscription.php
+++ pageinscription.php
@@ -7,23 +7,43 @@
 	<?php 
 require_once("connect_login.php");
 
-	if(!isset ($_POST['pseudo']) || !isset($_POST['motdepasse']) || !isset($_POST['motdepasse2']) || $_POST['motdepasse2']!=$_POST['motdepasse'] || $_POST['pseudo']=='' || $_POST['motdepasse']=='') { ?>
+	if(!isset($_POST['email']) || !isset ($_POST['pseudo']) || !isset($_POST['motdepasse']) || !isset($_POST['motdepasse2']) || $_POST['motdepasse2']!=$_POST['motdepasse'] || $_POST['pseudo']=='' || $_POST['motdepasse']=='' || $_POST['email']=='') { ?>
 	<table border="0">
 	<form method="POST" action="pageinscription.php">
 		<tr>
 			<td>Choisissez un pseudo : </td>
+			<?php  //si le pseudo n'est pas renseigné
+				if ($_POST['pseudo']=='') {
+					echo '<td><p class="erreur">Veuillez renseigner un pseudo</p></td>';
+				}
+			?>
 			<td><input type="TEXT" name="pseudo" placeholder="Pseudo"></td>
 		</tr>
 		<tr>
 			<td>Choisissez un mot de passe : </td>
+			<?php  //si le mot de passe n'est pas renseigné
+				if ($_POST['motdepasse']=='') {
+					echo '<td><p class="erreur">Veuillez renseigner un mot de passe</p></td>';
+				}
+			?>
 			<td><input type="password" name="motdepasse" placeholder="mot de passe" ></td>
 		</tr>
 		<tr>
 			<td>Confirmation du mot de passe : </td>
+			<?php  //si le mot de passe de confirmation n'est pas égal au mdp
+				if ($_POST['motdepasse2']!=$_POST['motdepasse']) {
+					echo '<td><p class="erreur">La confirmation ne correspond pas au mot de passe.</p></td>';
+				}
+			?>
 			<td><input type="password" name="motdepasse2" placeholder="mot de passe"></td>
 		</tr>
 		<tr>
 			<td>Adresse E-mail : </td>
+			<?php  //si l'email n'est pas renseigné
+				if ($_POST['email']=='') {
+					echo '<td><p class="erreur">Veuillez renseigner un email</p></td>';
+				}
+			?>
 			<td><input type="text" name="email" placeholder="adresse@domaine.tld"></td>
 		</tr>
 		<tr>
