d84d6490dfeaf0c9edf8bf296c6934aa1b8cd700
Infinita <infinita740@gmail.com>
2012-12-18 01:07:26 +0100

    menu style

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
+<!doctype html>
 <html>
 <head><title>Site du projet</title></head>
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
 <body>
 
 <?php
 echo "<h1>It works!</h1>";?>
 <nav>
-<?php
-include 'includes/menu.php';
-?>
+	<ul>
+		<a href='#'><li>Accueil</li></a>
+		<a href='#'><li>Nouveaut&eacute;s</li></a>
+		<a href='#'><li>Bestiaire</li></a>
+		<a href='#'><li>Boutique</li></a>
+		<a href='#'><li>Extras</li></a>
+		<a href='#'><li>Livre d'or</li></a>
+		<a href='#'><li>Contact</li></a>
 </nav>
 </body>
 </html>
