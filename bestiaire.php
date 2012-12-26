9ec220d878cee8720388d9aacefad598d7990163
Masterkdz <kevin.dezorzi@hotmail.fr>
2012-12-26 19:06:31 +0100

    changing mysql bestiaire..

--- bestiaire.php
+++ bestiaire.php
@@ -1,5 +1,6 @@
 <?php
-include("session_start.php")
+require_once("connect_login");
+include("session_start.php");
 include("entete.php");
 include("menu.php");
 if(!isset($_SESSION['pagebestiaire'])){
@@ -9,15 +10,22 @@ if(!isset($_SESSION['pagebestiaire'])){
 
 for($i=$_SESSION['pagebestiaire'];$i<$_SESSION['pagebestiaire']+5;$i++){
 	echo "<a href=article.php?id=".$i; 
-	echo ""; // mysql : titre de l'article (+photo) (l'id de l'article est $i)
-	echo "<br/>"
+	$sql="SELECT titre FROM articlesbestaire WHERE id ='$i'";
+	$query=mysql_query($sql);
+	echo $query ; // mysql : titre de l'article (+photo) (l'id de l'article est $i)
+	echo "<br/>";
 }
+
 $_SESSION['pagebestiaire']=$_SESSION['pagebestiaire']+5;
-if ($_SESSION['pagebestiaire']>1){
-	$_SESSION['pagebestiaire']=$_SESSION['pagebestiaire']-5;
-	echo "<a href='bestiaire.php'>Page précédente </a>";
+	if ($_SESSION['pagebestiaire']>1){
+    if($_SESSION['pagebestiaire']>5){
+    $_SESSION['pagebestiaire']=$_SESSION['pagebestiaire']-5;}
+    else {$_SESSION['pagebestiaire']=1;}
+    echo "<a href='bestiaire.php'>Page précédente </a>";
 }
-if($_SESSION)['pagebestiaire']</*mysql : compter le nb d'entrées dans la table articlesbestaire */ {
+
+}
+/* if($_SESSION)['pagebestiaire']</*mysql : compter le nb d'entrées dans la table articlesbestaire  {
 	echo "<a href'bestiaire.php'>Page suivante.</a>"
-	}
+	} */
 ?>
--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 	<ul>
 		<a href='index.php'><li><img src="img/testcaret.png">Accueil</li></a>
 		<a href='#'><li><img src="img/testcaret.png">Nouveaut&eacute;s</li></a>
-		<a href='Bestiaires.php'><li><img src="img/testcaret.png">Bestiaire</li></a>
+		<a href='Bestiaire.php'><li><img src="img/testcaret.png">Bestiaire</li></a>
 		<a href='Boutique0.php'><li><img src="img/testcaret.png">Boutique</li></a>
 		<a href='extras.php'><li><img src="img/testcaret.png">Extras</li></a>
 		<a href='livreor2.php'><li><img src="img/testcaret.png">Livre d'or</li></a>
