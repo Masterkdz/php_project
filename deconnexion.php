c9f7cac0082b1f4e4d67454235ce134823f2748f
Masterkdz <kevin.dezorzi@hotmail.fr>
2013-01-03 14:26:53 +0100

    changement deconnexion :D

--- deconnexion.php
+++ deconnexion.php
@@ -4,6 +4,6 @@ include("entete.php");
 include("menu.php");
 require_once("connect_login.php");
 session_destroy();
-echo "Vous &ecirc;tes d&eacute;connect&eacute;!<br>";
-echo "<a href='index.php'>Retour &agrave; l'accueil</a>";
+echo "<p class='p1'>Vous &ecirc;tes d&eacute;connect&eacute;!</p>";
+echo "<p class='centrer'><a class='bouton' href='index.php'>Retour &agrave; l'accueil</a></p>";
 ?>
