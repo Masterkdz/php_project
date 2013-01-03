9b2f9379bd9823f900526ebecfdac647c947633c
Infinita <infinita740@gmail.com>
2013-01-03 12:00:44 +0100

    css login corrigé + positionnement

--- entete.php
+++ entete.php
@@ -4,10 +4,9 @@
 <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
 <body>
-
-
-	<a href="index.php"><img src="img/image_accueil.jpg" height="150" width="300"></a>
 <?php
 include("session.php");
 ?>
+
+	<a href="index.php"><img src="img/image_accueil.jpg" height="150" width="300"></a>
 
