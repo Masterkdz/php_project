afaf3a1474338602fb29656450c8ae99b4a80768
Infinita <infinita740@gmail.com>
2013-01-04 18:12:08 +0100

    article.php fonctionne correctement maintenant

--- article.php
+++ article.php
@@ -2,7 +2,9 @@
 require_once("connect_login.php");
 include("entete.php");
 include("menu.php");
-$sql="SELECT article FROM articlesbestiaire WHERE id = 1";
+$idArticle=$_GET['id'];
+$typeArticle=$_SESSION['typearticle'];
+$sql="SELECT article FROM $typeArticle WHERE id =$idArticle";
 $resultat=qdb($sql);
 while ($row = mysql_fetch_assoc($resultat)) {
 echo $row["article"]; 
