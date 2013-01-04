afb3cd002e9bc1384e8cf3ad6ac19d93287e0437
Masterkdz <kevin.dezorzi@hotmail.fr>
2013-01-04 19:10:58 +0100

    acces extras

--- extras.php
+++ extras.php
@@ -3,6 +3,13 @@
 require_once("connect_login.php");
 include("entete.php");
 include("menu.php");
-echo "<h1>Vous trouverez ici divers extras que nous vous offrons!</h1><p><a href='classgenerator.php'>
+
+echo "<h1>Vous trouverez ici divers extras que nous offrons aux membres inscrits!</h1>";
+if (isset($_SESSION['pseudo'])){
+echo "<p><a href='classgenerator.php'>
 	Cliquez ici pour acc&eacute;der au premier ClassGenerator du jeu : Call of Duty 4 - Modern Warfare!</a></p>";
+}
+else{
+	echo "<p class=centrer>Vous n'&ecirc;tes apparemment pas inscrit ou bien non authentifi&eacute;. Cliquer <a href=pageinscription.php>ici</a> pour vous inscrire ou <a href=login.php>ici</a> pour vous logguer!</p>";
+}
 ?>
